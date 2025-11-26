a03aa5cc2bdd42aae11bd8884ef84c787039304c
Emanoel Silva <emanoelsilva1712@gmail.com>
2025-11-26 01:19:42 +0000

    contato

---
+++ tasks/poo/contato/py/draft.py
@@ -0,0 +1,10 @@
+class Contato:
+    def __init__(self), nome: str:
+        self.__nome = nome:
+        self.__fones: list<fone>
+        self.__favorited: bool
+
+class Fone:
+    def __init__(self):
+        self.__id: str
+        self.__number: int
